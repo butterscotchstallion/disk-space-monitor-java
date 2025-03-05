aee8cfe7a72a9c054b8d08f6719bba525cc248bd
Butterscotch Stallion <swift.plan5016@fastmail.com>
2025-03-04 20:49:34 -0500

    Hello JavaFX!

---
+++ DiskSpaceMonitor/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.dsm.diskspacemonitor {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.dsm.diskspacemonitor to javafx.fxml;
+    exports com.dsm.diskspacemonitor;
+}
